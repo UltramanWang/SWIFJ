799bb6ae458c6077ddf061dbab81b372e64b3d72
UltramanWang <156871160+UltramanWang@users.noreply.github.com>
2024-03-28 09:11:38 +0800

    12344444

--- source_code/UI/main.py
+++ source_code/UI/main.py
@@ -3,11 +3,7 @@ import os
 from PyQt5.QtWidgets import QApplication
 from mainwindow import MainWindow
 
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# from sudu.gdb.other import print_test
-
 if __name__ == "__main__":
-    # print_test()
     app = QApplication(sys.argv)
     win = MainWindow()
     win.show()
